164b8571b9cebc4fd7718b807a1d5a8e7910416b
gutianchenhao <13599833622@163.com>
2020-04-04 23:56:47 +0800

    23:56

--- src/main/java/com/ccb/ShareWebHostingApplication.java
+++ src/main/java/com/ccb/ShareWebHostingApplication.java
@@ -13,7 +13,7 @@ public class ShareWebHostingApplication{
 
     @RequestMapping("/")
     public String hello(){
-        return "hello auto build of jenkins";
+        return "1hello auto build of jenkins";
     }
 }
 
