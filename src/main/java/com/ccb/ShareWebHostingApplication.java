95d97390c3f1d9adec75eaa15779ca87cd1937df
gutianchenhao <13599833622@163.com>
2020-04-04 23:45:28 +0800

    23:45

--- src/main/java/com/ccb/ShareWebHostingApplication.java
+++ src/main/java/com/ccb/ShareWebHostingApplication.java
@@ -1,11 +1,19 @@
 package com.ccb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class ShareWebHostingApplication{
     public static void main(String[] args) {
-	System.out.println("hell world!");
         SpringApplication.run(ShareWebHostingApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello auto build of jenkins";
     }
 }
 
