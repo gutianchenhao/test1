a2ba8d008c527e6b42eb2a37ac0974bc1df2cd80
gutianchenhao <13599833622@163.com>
2020-04-04 17:47:02 +0800

    17:46_4_4

---
+++ src/main/java/com/ccb/ShareWebHostingApplication.java
@@ -0,0 +1,10 @@
+package com.ccb;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ShareWebHostingApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(ShareWebHostingApplication.class, args);
+    }
+}
+
