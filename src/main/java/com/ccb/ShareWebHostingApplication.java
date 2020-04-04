47e4ee1ea428e063ef00e59f739e9cd88adc86a1
gutianchenhao <13599833622@163.com>
2020-04-04 18:33:15 +0800

    18:33:15

--- src/main/java/com/ccb/ShareWebHostingApplication.java
+++ src/main/java/com/ccb/ShareWebHostingApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShareWebHostingApplication{
     public static void main(String[] args) {
+	System.out.println("hello world!");
         SpringApplication.run(ShareWebHostingApplication.class, args);
     }
 }
